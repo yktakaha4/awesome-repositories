4a6fab1d8d154775e7d77e174600ae1ba008e55f
yk.takaha4 <yk.takaha4@gmail.com>
2018-09-22 06:34:01 +0000

    アプリ起動時にDBが存在しない場合を考慮

--- config/initializers/initialize_application.rb
+++ config/initializers/initialize_application.rb
@@ -4,22 +4,22 @@ logger = Rails.logger
 
 if ENV["APP_START_FROM"].nil?
   logger.info "initializing application..."
-  
-  Rails.application.load_tasks
-  
-  RepositoryCollection.all_enabled.each do |collection|
-    id = collection.repository_collection_setting.id
-    begin
-      Rake::Task["crawl_collections:make_autocomplete"].reenable
-      Rake::Task["crawl_collections:make_autocomplete"].invoke(id)  
-      logger.info "succeed to invoke: crawl_collections:make_autocomplete, id=#{id}"
-    rescue
-     logger.error "failed to invoke: crawl_collections:make_autocomplete, id=#{id}"
-    end
 
-  end
-  
   begin
+    Rails.application.load_tasks
+    
+    RepositoryCollection.all_enabled.each do |collection|
+      id = collection.repository_collection_setting.id
+      begin
+        Rake::Task["crawl_collections:make_autocomplete"].reenable
+        Rake::Task["crawl_collections:make_autocomplete"].invoke(id)  
+        logger.info "succeed to invoke: crawl_collections:make_autocomplete, id=#{id}"
+      rescue
+       logger.error "failed to invoke: crawl_collections:make_autocomplete, id=#{id}"
+      end
+  
+    end
+  
     MonitorMailer.send_monitor_mail("Server startup").deliver!
   rescue => e
     logger.warn "failed to send mail..."
